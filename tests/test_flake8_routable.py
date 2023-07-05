eec1e4b4c6095f943cb4bf48b1526755e81edf5b
Tom Harel <3478172+tomharel@users.noreply.github.com>
2023-07-05 15:28:33 -0700

    add rule for disallowing migration renames

--- tests/test_flake8_routable.py
+++ tests/test_flake8_routable.py
@@ -506,6 +506,40 @@ class TestROU108:
         assert error == set()
 
 
+class TestROU109:
+    ADD_MIGRATION = (
+    """class Migration(migrations.Migration):
+        dependencies = []
+        operations = [
+            migrations.AddField(
+                model_name="model_one",
+                name="swift_charge_option",
+                field=models.TextField(blank=True, null=True),
+            )
+        ]"""
+    )
+
+    RENAME_MIGRATION = (
+    """class Migration(migrations.Migration):
+        dependencies = []
+        operations = [
+            migrations.RenameField(
+                model_name="model_one",
+                old="field_one",
+                new="field_two",
+            )
+        ]"""
+    )
+
+    def test_correct_no_import_from_tests(self):
+        errors = results(self.ADD_MIGRATION)
+        assert errors == set()
+
+    def test_incorrect_import_from_tests(self):
+        errors = results(self.RENAME_MIGRATION)
+        assert errors == {"4:12: ROU109 Disallow rename migrations"}
+
+
 class TestVisitor:
     def test_parse_to_string_warning(self):
         visitor = Visitor()
